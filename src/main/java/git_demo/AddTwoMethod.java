db77914e4c7523c31ade5d50309d8c7b3ef424a7
khajasayyed10 <khajasayyed10@gmail.com>
2024-04-18 00:09:45 +0530

    added method double

--- src/main/java/git_demo/AddTwoMethod.java
+++ src/main/java/git_demo/AddTwoMethod.java
@@ -15,12 +15,17 @@ public class AddTwoMethod extends App{
         return "khaja";
     }
 
+    public double aDouble(){
+        return 12354.0;
+    }
+
     public static void main(String[] args) {
         AddTwoMethod addTwoMethod= new AddTwoMethod();
 //        addTwoMethod.addedMethod();
         App.addedMethod();
         System.out.println(addTwoMethod.addNumber(12,45));
         System.out.println(addTwoMethod.name());
+        System.out.println(addTwoMethod.aDouble());
 
     }
 }
