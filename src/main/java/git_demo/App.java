13bdeed628e4f8f74ce43ed870fe502b60259777
khajasayyed10 <khajasayyed10@gmail.com>
2024-04-17 16:35:32 +0530

    first commit to hello world app

---
+++ src/main/java/git_demo/App.java
@@ -0,0 +1,13 @@
+package git_demo;
+/*
+Hello world
+*/
+public class App {
+
+    public static void add(String data){
+        System.out.println("data"+data);
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
