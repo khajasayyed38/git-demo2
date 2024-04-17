a57adc4943c94978987e4bf89655065950a39cdd
khajasayyed38 <92858743+khajasayyed38@users.noreply.github.com>
2024-04-17 16:52:35 +0530

    added a method

--- src/main/java/git_demo/App.java
+++ src/main/java/git_demo/App.java
@@ -6,6 +6,9 @@ public class App {
             System.out.println("welcome");
         }
     }
+    public void addedMethod(){
+        System.out.println("welcome");
+    }
     public static void main(String[] args) {
         System.out.println("Hello world");
         add("java techie");
