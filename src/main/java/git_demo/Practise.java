6e91d1b062fc09c18b5147bf1af4cab74caf1e1f
khajasayyed10 <khajasayyed10@gmail.com>
2024-04-18 12:13:01 +0530

    added a method

--- src/main/java/git_demo/Practise.java
+++ src/main/java/git_demo/Practise.java
@@ -1,7 +1,13 @@
 package git_demo;
 
 public class Practise {
+    public static void addNumber(int a,  int b){
+
+        System.out.println(a+b);
+    }
     public static void main(String[] args) {
+
         System.out.println("Practising the git");
+        addNumber(12,45);
     }
 }
